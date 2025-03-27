4eb2b990f4b4d77a77a11638d47bf9778e6e55d3
szaboistvanckik <szabo.istvan@ckik.hu>
2025-03-27 10:34:40 +0100

    mozgatas alap

--- event.js
+++ event.js
@@ -1,6 +1,7 @@
 let currentPlayer;
 let whiteCount;
 let blackCount;
+let selectedPiece;
 let alerttriggered = false;
 let positions = [
     [2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
@@ -18,12 +19,16 @@ let positions = [
 
 
 function generate() {
+    alerttriggered = false;
     const board = document.querySelector("table");
     board.innerHTML = "";
+    document.querySelector("input").value = "Reset";
     currentPlayer = 1;
     whiteCount = 0;
     blackCount = 0;
     document.getElementById("playerText").innerText = "1. Játékos: ⚪";
+    document.getElementById("egy").innerText = "1. Lerakható ⚪ még: " + 9;
+    document.getElementById("ketto").innerText = "2. Lerakható ⚫ még: " + 9;
 
     const table = [
         [0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0],
@@ -37,15 +42,40 @@ function generate() {
     gen_table(board, table);
 }
 
+
 function move(cell) {
-    console.log("mozog");
+    const row = cell.parentNode.rowIndex;
+    const col = cell.cellIndex;
+
+        if ((currentPlayer == 1 && positions[row][col] == 5) || (currentPlayer == 2 && positions[row][col] == 7)) {
+            console.log("selected" + row + ", " + col);
+            selectedPiece = cell;
+            highlightMoves(row, col);
+        } else {
+            console.log("invalid");
+        }
 }
 
-// for (let i = 0; i < positions.length; i++) {
-//     for (let j = 0; j < positions[i].length; j++) {
-//         console.log(positions[i][j])
-//     }
-// }
+function highlightMoves() {
+    // [2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
+    // [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    // [1, 0, 2, 1, 1, 2, 1, 1, 2, 0, 1],
+    // [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1],
+    // [1, 0, 1, 0, 2, 2, 2, 0, 1, 0, 1],
+    // [2, 1, 2, 1, 2, 0, 2, 1, 2, 1, 2],
+    // [1, 0, 1, 0, 2, 2, 2, 0, 1, 0, 1],
+    // [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1],
+    // [1, 0, 2, 1, 1, 2, 1, 1, 2, 0, 1],
+    // [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    // [2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2],
+    // del --> elso koord +1
+    // eszak --> elso kord -1
+    // kelet --> masodik kord + 1
+    // nyugat --> masodik kord -1;
+    if (condition) {
+        
+    }
+}
 
 function gen_table(board, table) {
     for (let i = 0; i < 11; i++) {
@@ -105,14 +135,14 @@ function place(cell) {
     if (currentPlayer == 1) {
         img.src = "white.png";
         whiteCount++;
-        document.getElementById("egy").innerText = "1. Lerakható még: " + (9 - whiteCount);
+        document.getElementById("egy").innerText = "1. Lerakható ⚪ még: " + (9 - whiteCount);
         currentPlayer = 2;
         document.getElementById("playerText").innerText = "2. Játékos: ⚫";
         positions[row][col] = 5;
     } else {
         img.src = "black.png";
         blackCount++;
-        document.getElementById("ketto").innerText = "2. Lerakható még: " + (9 - blackCount);
+        document.getElementById("ketto").innerText = "2. Lerakható ⚫ még: " + (9 - blackCount);
         currentPlayer = 1;
         document.getElementById("playerText").innerText = "1. Játékos: ⚪";
         positions[row][col] = 7;
